acd5340d6a8ffbe4678bb761f1b3b9df8b76195c
keigodasu <keigodasu0524@yahoo.co.jp>
2020-12-20 10:49:45 +0900

    solve

--- database/second-highest-salary.sql
+++ database/second-highest-salary.sql
@@ -1,19 +1,3 @@
-SELECT
-  MAX(Salary) AS SecondHighestSalary
-FROM
-  Employee
-WHERE
-  Salary = (
-    SELECT
-      MAX(Salary)
-    FROM
-      Employee
-    WHERE
-      Salary < (
-        SELECT
-          MAX(salary)
-        FROM
-          Employee
-      )
-  );
-
+SELECT (SELECT DISTINCT Salary
+FROM Employee
+ORDER BY Salary DESC LIMIT 1 OFFSET 1) AS SecondHighestSalary
