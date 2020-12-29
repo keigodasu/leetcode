37f3e0894f4925f795e2fd18ab5d6d420abdab6d
keigodasu <keigodasu0524@yahoo.co.jp>
2020-12-29 15:03:36 +0900

    solve

--- database/top-travellers.sql
+++ database/top-travellers.sql
@@ -6,3 +6,9 @@ users u, rides r
 group by u.id
 order by travelled_distance desc, name asc;
 
+select u.name, ifnull(sum(r.distance),0) travelled_distance
+from users as u
+left join rides as r
+on u.id = r.user_id
+group by u.name
+order by travelled_distance desc, u.name;
