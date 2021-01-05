10fabe549cc601d89eb72d5a45d9af8e31ba5721
keigodasu <keigodasu0524@yahoo.co.jp>
2021-01-05 23:38:43 +0900

    solve

---
+++ database/the-most-recent-orders-for-each-product.sql
@@ -0,0 +1,22 @@
+select
+    product_name,
+    product_id,
+    order_id,
+    order_date
+from
+(
+select
+    product_name,
+    product_id,
+    order_id,
+    order_date,
+    rank() over(partition by product_id order by order_date desc) rnk
+from
+    orders
+    join
+    products
+    using(product_id)
+) t
+where
+    rnk = 1
+order by product_name asc, product_id asc, order_id asc
