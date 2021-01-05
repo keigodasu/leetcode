ec1a8edf2ec46534d92c42af0d83df311956c39e
keigodasu <keigodasu0524@yahoo.co.jp>
2021-01-05 23:43:28 +0900

    solve

--- database/the-most-recent-orders-for-each-product.sql
+++ database/the-most-recent-orders-for-each-product.sql
@@ -19,4 +19,28 @@ from
 ) t
 where
     rnk = 1
-order by product_name asc, product_id asc, order_id asc
+order by product_name asc, product_id asc, order_id asc;
+
+
+with orders_with_rank as (
+select
+    product_name,
+    product_id,
+    order_id,
+    order_date,
+    rank() over(partition by product_id order by order_date desc) rnk
+from
+    orders
+    join
+    products
+    using(product_id)
+)
+select
+    product_name,
+    product_id,
+    order_id,
+    order_date
+from orders_with_rank
+where
+    rnk = 1
+order by product_name, product_id, order_id;
