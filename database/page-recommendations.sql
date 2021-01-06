ac908aa55ce63006ee68820dce53a8833de92ece
keigodasu <keigodasu0524@yahoo.co.jp>
2021-01-07 00:25:55 +0900

    solve

---
+++ database/page-recommendations.sql
@@ -0,0 +1,48 @@
+select
+distinct page_id recommended_page
+from
+likes
+where 
+user_id in 
+(
+select 
+    user2_id
+from
+   Friendship
+where
+    user1_id = 1
+union
+select 
+    user1_id
+from
+   Friendship
+where
+    user2_id = 1
+)
+and
+page_id not in
+(
+select
+    page_id
+from
+    likes
+where
+    user_id = 1
+);
+
+select
+distinct page_id recommended_page
+from
+(
+select
+    case 
+        when user1_id = 1 then user2_id
+        when user2_id = 1 then user1_id
+    end user_id
+from friendship
+) a
+join
+likes
+using(user_id)
+where
+page_id not in (select page_id from likes where user_id = 1);
