0c1804bdcdab82f78d004820fbe89f725ea8cb8f
keigodasu <keigodasu0524@yahoo.co.jp>
2021-01-06 09:25:14 +0900

    solve

---
+++ database/monthly-transactions-1.sql
@@ -0,0 +1,21 @@
+select
+left(trans_date, 7) month,
+country,
+count(*) trans_count,
+sum(case when state = 'approved' then 1 else 0 end) approved_count,
+sum(amount) trans_total_amount,
+sum(case when state = 'approved' then amount else 0 end) approved_total_amount
+from
+transactions
+group by left(trans_date, 7), country;
+
+select
+left(trans_date, 7) month,
+country,
+count(*) trans_count,
+sum(state = 'approved') approved_count,
+sum(amount) trans_total_amount,
+sum(case when state = 'approved' then amount else 0 end) approved_total_amount
+from
+transactions
+group by left(trans_date, 7), country;
