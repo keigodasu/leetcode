a0ad556862925a15a98c0d5e3ecaaa2c16e53803
keigodasu <keigodasu0524@yahoo.co.jp>
2021-01-13 10:10:19 +0900

    solve

--- algorithms/average-salary-excluding-the-minimum-and-maximum-salary.go
+++ algorithms/average-salary-excluding-the-minimum-and-maximum-salary.go
@@ -33,3 +33,16 @@ func findMinSalary(salary []int) int{
     
     return min
 }
+
+///////
+
+func average(salary []int) float64 {
+    total, min, max := 0, 100000000, 1000
+    for _, v := range salary {
+        total += v
+        if v < min { min = v }
+        if v > max { max = v }
+    }
+
+    return float64(total - max -min) / float64(len(salary) - 2)
+}
