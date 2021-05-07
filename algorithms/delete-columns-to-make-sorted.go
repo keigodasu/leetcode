d5f3f3a9ece240e4fcbd960bd9bfa95ef3dc8127
keigodasu <keigodasu05240524@gmail.com>
2021-05-07 21:33:14 +0900

    solve

---
+++ algorithms/delete-columns-to-make-sorted.go
@@ -0,0 +1,13 @@
+func minDeletionSize(A []string) int {
+    stringLen := len(A)
+    cn := 0
+    for i := 0; i < len(A[0]); i++ {
+        for k:=0; k < stringLen - 1; k++ {
+            if A[k][i] > A[k+1][i] {
+                cn++
+                break
+            }
+        }
+    }
+    return cn
+}
