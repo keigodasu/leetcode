cad964b0652b42cd13f53bd757ff67e1cb6984d0
keigodasu <keigodasu05240524@gmail.com>
2021-05-22 23:12:35 +0900

    solve

---
+++ algorithms/design-hash-map.py
@@ -0,0 +1,28 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.bucket = []
+        
+
+    def put(self, key: int, value: int) -> None:
+        found = False
+        for i, kv in enumerate(self.bucket):
+            if key == kv[0]:
+                self.bucket[i] = (key, value)
+                found = True
+                break
+        if not found:
+            self.bucket.append((key, value))
+
+    def get(self, key: int) -> int:
+        for (k, v) in self.bucket:
+            if k == key:
+                return v
+        return -1
+        
+
+    def remove(self, key: int) -> None:
+        for i, kv in enumerate(self.bucket):
+            if key == kv[0]:
+                del self.bucket[i]
+
