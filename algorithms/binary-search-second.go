5de1eebb7de2f2b2e1aa30638614e4947d72fe5a
keigodasu <keigodasu05240524@gmail.com>
2021-05-30 21:45:12 +0900

    solve

---
+++ algorithms/binary-search-second.go
@@ -0,0 +1,18 @@
+func search(nums []int, target int) int {
+    left := 0
+    right := len(nums) - 1
+    
+    for left <= right {
+        //offsetとしてleftに足される
+        mid := left + (right - left) / 2
+        if nums[mid] == target {
+            return mid
+        } else if nums[mid] > target {
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+   
+    return -1
+}
