3d21f3eec707a9b1c447ac7e07a8ab6ddf6699ee
keigodasu <keigodasu0524@yahoo.co.jp>
2022-01-29 11:59:59 +0900

    solved

---
+++ database/second_round/users-that-actively-request-confirmation-messages.sql
@@ -0,0 +1,7 @@
+SELECT
+DISTINCT c1.user_id
+FROM confirmations c1
+JOIN confirmations c2
+ON c1.user_id = c2.user_id
+AND c1.time_stamp > c2.time_stamp
+AND TIMESTAMPDIFF(second, c2.time_stamp, c1.time_stamp) <= 86400;
