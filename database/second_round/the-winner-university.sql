38699bac16de16cc82b4a684fb60fb839d0ef4fb
keigodasu <keigodasu0524@yahoo.co.jp>
2022-02-05 16:49:06 +0900

    solved

---
+++ database/second_round/the-winner-university.sql
@@ -0,0 +1,2 @@
+SELECT
+(case when (select count(*) from NewYork where score >= 90) > (select count(*) from California where score >= 90) then 'New York University' when (select count(*) from NewYork where score >= 90) < (select count(*) from California where score >= 90) then 'California University' else 'No Winner' END) AS winner;
